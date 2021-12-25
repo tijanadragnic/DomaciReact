de597e45556cb32985cf2c9170bad46bf32dbb9f
tijanadragnic <tijanadragnic98@gmail.com>
2021-12-25 23:28:57 +0100

    mymessage

--- zadatak1/app.js
+++ zadatak1/app.js
@@ -1,2 +1,9 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import FormComponent from "./form-component";
+
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<FormComponent />, rootElement);
 
