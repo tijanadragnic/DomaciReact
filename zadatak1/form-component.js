a4589aa5502aaff059529fe31b995e6d78b02717
tijanadragnic <tijanadragnic98@gmail.com>
2021-12-26 14:32:00 +0100

    state

--- zadatak1/form-component.js
+++ zadatak1/form-component.js
@@ -1,1 +1,32 @@
 import React from "react";
+
+const emailValidator = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordValidator=/^[a-zA-Z0-9!@#$%^&*]{8,}$/;
+
+class FormComponent extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            userName="",
+            firstName="",
+            lastName="",
+            emailAdress="",
+            password="",
+            passwordConfirmation="",
+            userNameError="",
+            emailAdressError="",
+            passwordError="",
+            passwordConfirmationError="",
+            isFormSubmitted=false
+
+        };
+        
+
+    }
+ 
+
+
+
+
+
+}
