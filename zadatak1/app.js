67eaa0e7019d397f0338c2317e38901d433b8697
Aler97 <radicevicsasa836@gmail.com>
2021-12-29 13:22:16 +0100

    file structure

--- zadatak1/app.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import FormComponent from "./form-component.js";
-
-import "./styles.css";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<FormComponent />, rootElement);
-
---
+++ zadatak1/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+import FormComponent from './formComponent';
+
+function App() {
+  return (<FormComponent />)
+
+}
+
+export default App;
