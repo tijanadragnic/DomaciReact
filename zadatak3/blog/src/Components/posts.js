821c2b151c381cd04ed20ff44a7be66c008b6a40
Aler97 <radicevicsasa836@gmail.com>
2022-01-05 16:02:53 +0100

    style

--- zadatak3/blog/src/Components/posts.js
+++ zadatak3/blog/src/Components/posts.js
@@ -11,6 +11,7 @@ class Posts extends React.Component {
         super(props);
         this.state = {
             posts: [],
+            error: false,
         }
     }
 
@@ -21,12 +22,15 @@ class Posts extends React.Component {
 
             this.setState({ posts: [...response.data] })
         } catch (err) {
+            this.setState({ error: true })
             console.log(err)
         }
     }
 
     render() {
-        return (this.state.posts.length > 0 ? <div className='container'> {this.state.posts.map((post) => { return (<Link key={post.id} to={`/${post.id}`} className='titleLink'><div><img src={post.image_url} alt="img" /><div className='postText'><h1>{post.title}</h1><h2>By: {post.author}</h2><p>{post.content}</p></div></div></Link>) })}</div> : <div className='loadingPosts'><p>Loading Posts...</p></div>)
+
+        return (this.state.error ? <div className='errorMessage'><h1>Error Occured. Please refresh the page!</h1><button onClick={() => { window.location.reload() }}>Refresh</button></div> : this.state.posts.length > 0 ? <div className='container'> {this.state.posts.map((post) => { return (<Link key={post.id} to={`/${post.id}`} className='titleLink'><div><img src={post.image_url} alt="img" /><div className='postText'><h1>{post.title}</h1><h2>By: {post.author}</h2><p>{post.content}</p></div></div></Link>) })}</div> : <div className='loadingPosts'><p>Loading Posts...</p></div>)
+
     }
 }
 
